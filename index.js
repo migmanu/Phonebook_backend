bf17add50fece20824f40ee7943d6379e041712e
José Manuel Migoya Bussolotti <52668755+migmanu@users.noreply.github.com>
2021-07-22 10:11:43 +0200

    Phonebook_backend completed up to exercise 3.1 (step1)

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+    { 
+        "id": 1,
+        "name": "Arto Hellas", 
+        "number": "040-123456"
+      },
+      { 
+        "id": 2,
+        "name": "Ada Lovelace", 
+        "number": "39-44-5323523"
+      },
+      { 
+        "id": 3,
+        "name": "Dan Abramov", 
+        "number": "12-43-234345"
+      },
+      { 
+        "id": 4,
+        "name": "Mary Poppendieck", 
+        "number": "39-23-6423122"
+      }
+]
+
+app.get('/', (request, response) => {
+    console.log('get src init');
+    response.send('<h1>Hello World!</h1>');
+})
+
+app.get('/api/persons', (request, response) => {
+    console.log('get persons init');
+    response.json(persons)
+})
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`);
